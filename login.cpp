0b46ae75191e738986d65ef050acaf37918b635f
salbi <mohamedsali602@live.com>
2020-12-15 12:43:27 +0100

    This one is like the backbone of the script it still needs some ecryption or hash function  to make it a decent login interface

---
+++ login.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class loginfo
+{
+public:
+    void login(void)
+    {
+        cout<<"Enter Username: "<<endl;
+        cin>>userTyped;
+        if(userTyped==defaultUser)
+        {
+            cout<<"\n";
+            cout<<"Enter Password: "<<endl;
+            cin>>passwordTyped;
+            if(passwordTyped==defaultPassword)
+            {
+                acess=true;
+                cout<<"Welcome xD!"<<endl;
+            }
+            else
+            {
+                cout<<"Password Wrong !"<<endl;
+                if(passwordTyped.length()>defaultPassword.length())
+                {
+                    cout<<"Too long!"<<endl;
+                }
+                else if(passwordTyped.length()<defaultPassword.length())
+                {
+                    cout<<"Too short!"<<endl;
+                }
+                
+                login();
+            } 
+        }
+        else
+        {
+            cout<<"Who You're kidding !"<<endl;
+            login();
+        }  
+    }     
+    string getFile(const char* p_fileName)
+    {
+        string line;
+        fstream file;
+
+        file.open(p_fileName, ios::in);
+        if(file.is_open())
+        {
+            getline(file, line);
+        }
+        file.close();
+
+        return line;
+        
+    }
+private:
+    string defaultUser="user";
+    string defaultPassword="momo";
+    string userTyped;
+    string passwordTyped;
+    bool acess=true;
+};
+int main(int argc,char** argv)
+{
+    loginfo logobj;
+    logobj.login();
+    return 0;
+}
